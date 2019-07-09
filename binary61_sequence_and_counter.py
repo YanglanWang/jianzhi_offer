83cfc0a49cc9a3c4b8a8eed39cf3b0d8a530bb4b
yanglan <yanglan-17@mails.tsinghua.edu.cn>
2019-07-10 03:13:04 +0800

    ~1/2

--- binary61_sequence_and_counter.py
+++ binary61_sequence_and_counter.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.left = None
         self.right = None
 class Solution:
+    flag=-1
     def Serialize(self, root):
         # write code here
         if root==None:
@@ -17,33 +18,40 @@ class Solution:
         result.extend(right)
         return result
 
-
+    i=-1
     def Deserialize(self, s):
         # write code here
-        nodelist=[]
-        for i in s:
-            if i!='$':
-                nodelist.append(TreeNode(int(i)))
+        self.i+=1
+        if self.i<len(s):
+            if s[self.i]!='$':
+                root=TreeNode(int(s[self.i]))
+                root.left=self.Deserialize(s)
+                root.right=self.Deserialize(s)
             else:
-                nodelist.append(None)
-        nodestart=nodelist[0]
-        nodestart.left=
+                return None
 
-        for i in range(len(nodelist)):
-            # if 2*i+1<len(nodelist):
-            if nodelist[i]!=None:
-                nodelist[i].left=nodelist[i+1]
-            else:
-                nodelist[i-1].right=
-            # else:
-            #     nodelist[i].left=None
-            if 2+i+2<len(nodelist):
-                nodelist[i].right=nodelist[2*i+2]
-            else:
-                nodelist[i].left = None
-        return nodelist[0]
+        return root
 
-    def createTree(self,tree):
+    #
+    # def createTree(self,nodelist):
+    #     if len(nodelist)==1 and nodelist[0]==None:
+    #         return None
+    #     nodelist[0].right=self.createTree(nodelist[1:])
+    #     nodelist[0].left=self.createTree(nodelist[1:])
+    #     return nodelist[0]
+
+    # def Deserialize(self, s):
+    #     self.flag += 1
+    #     # l = s.split(',')
+    #     l=s
+    #     if (self.flag >= len(s)):
+    #         return None
+    #     root = None
+    #     if (l[self.flag] != '$'):
+    #         root = TreeNode(int(l[self.flag]))
+    #         root.left = self.Deserialize(s)
+    #         root.right = self.Deserialize(s)
+    #     return root
 
 
 root=create_tree.fromList([8,6,10,5,7,9,11])
