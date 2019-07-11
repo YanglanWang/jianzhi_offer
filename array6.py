71fa5c75434cf95ecde75e6a6c97389434e9f516
yanglan <yanglan-17@mails.tsinghua.edu.cn>
2019-07-12 01:10:52 +0800

    7.12

--- array6.py
+++ array6.py
@@ -2,13 +2,25 @@
 class Solution:
     def minNumberInRotateArray(self, rotateArray):
         # write code here
-        if len(rotateArray) == 0:
+        if len(rotateArray)==0:
             return None
-        low = 0
-        high = len(rotateArray)-1
-        medium = int((low+high) / 2)
-        if rotateArray[medium]>rotateArray[high]:
-            low=medium
-            medium=int((low+high)/2)
-        else:
-            
+
+        # low=0
+        # high=len(rotateArray)-1
+        # while high-low!=1:
+        #     medium=int((low+high)/2)
+        #     if rotateArray[low]<rotateArray[medium]:
+        #         low=medium
+        #     else:
+        #         high=medium
+        # return rotateArray[high]
+        mini=1e-10
+        for i in rotateArray:
+            if i<mini:
+                return i
+            mini=i
+        return rotateArray[0]
+
+a=Solution()
+b=a.minNumberInRotateArray([1,1,1,0,1])
+print(b)
